0a6494f8ece92bb92f1d22c3e84b3aa6662ae395
Joyce Wu <joycewu@dhcp-vl2042-3886.redrover.cornell.edu>
2023-03-24 14:30:50 -0400

    Uncommented last line so can use app.py to start server

--- backend/app.py
+++ backend/app.py
@@ -12,7 +12,7 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
 # Don't worry about the deployment credentials, those are fixed
 # You can use a different DB name if you want to
 MYSQL_USER = "root"
-MYSQL_USER_PASSWORD = "MayankRao16Cornell.edu"
+MYSQL_USER_PASSWORD = ""
 MYSQL_PORT = 3306
 MYSQL_DATABASE = "kardashiandb"
 
@@ -37,9 +37,10 @@ def sql_search(episode):
 def home():
     return render_template('base.html',title="sample html")
 
-@app.route("/episodes")
+@app.route("/episodes", methods = ["POST"])
 def episodes_search():
-    text = request.args.get("title")
+    body = json.loads(request.data)
+    text = body.get("title")
     return sql_search(text)
 
-# app.run(debug=True)
+app.run(debug=True)
