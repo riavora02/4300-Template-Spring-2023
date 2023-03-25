7ebe23552d4ac6966eb06c1154f0673be895d17f
mluo24 <miranda.luo24@gmail.com>
2023-03-25 03:37:14 -0400

    added vue for testing

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Webtoon from './components/Webtoon.vue';
+</script>
+
+<template>
+  <div className="container mx-auto p-6">
+    <h1 className="text-3xl mb-3">Hello, world!</h1>
+    <p>This is a paragraph.</p>
+    <Webtoon title="hello" summary="hello" />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ frontend/src/components/Webtoon.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+interface WebtoonProps {
+    title: string
+    summary: string
+}
+
+defineProps<WebtoonProps>()
+
+</script>
+
+<template>
+    <div>
+        <h3>{{ title }}</h3>
+        <p>{{ summary }}</p>
+    </div>
+</template>
