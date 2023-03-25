cdb8056f6b8e9def501a8db2b459deb78b85805d
Miranda Luo <69130822+mluo24@users.noreply.github.com>
2023-03-25 16:50:29 -0400

    update init.sql with database use

--- init.sql
+++ init.sql
@@ -19,6 +19,10 @@
 -- Table structure for table `webtoons`
 --
 
+CREATE DATABASE IF NOT EXISTS kardashiandb;
+
+USE kardashiandb;
+
 DROP TABLE IF EXISTS `webtoons`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
