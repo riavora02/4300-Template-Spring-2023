f8f9fedd4b206039adc56c79e8cba6e4f3b28c34
mluo24 <miranda.luo24@gmail.com>
2023-03-25 04:18:13 -0400

    pulls from backend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,29 @@
 <script setup lang="ts">
+import axios from 'axios';
 import Webtoon from './components/Webtoon.vue';
-import { WebtoonProps } from './types';
+import { WebtoonType } from './types';
+import { onMounted, ref } from 'vue';
 
-const webtoonData: WebtoonProps[] = [
-  {
-    id: 1,
-    title: "Webtoon 1",
-    summary: "this is webtoon 1",
-  },
-  {
-    id: 2,
-    title: "Webtoon 2",
-    summary: "this is webtoon 2",
-  },
-]
+const BACKEND_URL = "http://127.0.0.1:8000"
+
+const webtoons = ref<WebtoonType[]>()
+
+onMounted(async () => {
+  axios.get(`${BACKEND_URL}/webtoons`).then((res) => {
+    console.log(res.data)
+    webtoons.value = res.data
+  }).catch((err) => {
+    console.log(err)
+  })
+})
+
 </script>
 
 <template>
   <div className="container mx-auto p-6">
     <h1 className="text-3xl mb-3">Hello, world!</h1>
     <p>This is a paragraph.</p>
-    <Webtoon v-for="webtoon in webtoonData" :id="webtoon.id" :title="webtoon.title" :summary="webtoon.summary" />
+    <Webtoon v-for="webtoon in webtoons" :id="webtoon.id" :title="webtoon.title" :summary="webtoon.summary" />
   </div>
 </template>
 
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -3,3 +3,11 @@ export interface WebtoonProps {
     title: string
     summary: string
 }
+export interface WebtoonType {
+    id: number
+    title: string
+    summary: string
+    genre: string
+    thumbnail: string
+    webtoon_id: number
+}
