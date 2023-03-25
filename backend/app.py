9ccde8b7c8723af1fd4bbf33494d0fde9d4dc49e
Miranda Luo <69130822+mluo24@users.noreply.github.com>
2023-03-25 16:26:26 -0400

    removed app.run

--- backend/app.py
+++ backend/app.py
@@ -49,5 +49,5 @@ def webtoon_search():
 #     text = request.args.get("title")
 #     return sql_search(text)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
