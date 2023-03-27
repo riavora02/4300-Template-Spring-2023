5be2b97c8127953748573fa2fd9c72dbfb157a22
Hannah Daniel <hcd38@cornell.edu>
2023-03-27 15:11:06 -0400

    cos algo modifications

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 import json
 import os
+from sklearn import TfidfVectorizer 
 from flask import Flask, render_template, request
 from flask_cors import CORS
 from helpers.MySQLDatabaseHandler import Webtoon, MySQLDatabaseHandler, db
@@ -53,8 +54,50 @@ def failure_response(error, code=404):
 #     print(data)
 #     return json.dumps([dict(zip(keys,i)) for i in data])
 
+def webtoon_name_array(data):
+    webtoon_to_index = {}
+    index = 0
+    for webtoon in data:
+        title = webtoon["title"]
+        webtoon_to_index[title] = index
+        index += 1
+
+
+def unique_words(data):
+    """Finds all the unique words in the webtoon summaries"""
+    unique = set()
+    for webtoon in data:
+        summary = webtoon["summary"]
+        tokened = set(summary.split())
+        unique = unique.union(tokened)
+
+
+def webtoon_tfdi(data, max_df=0.8, min_df=10, norm='l2',):
+    """word_count is the total number of unique words in the summaries,
+and data is the data from kaggle"""
+    word_count = unique_words(data)
+    vector = TfidfVectorizer(max_features=word_count,
+                             max_df=max_df,
+                             min_df=min_df,
+                             stop_words="english",
+                             norm=norm
+                             )
+    doc_by_vocab = vector.fit_transform([d['summary'] for d in data]).toarray()
+    return doc_by_vocab
+
+
+def get_cossim(query, wt, data):
+    matrix = webtoon_tfdi(data)
+    webtoon_to_index = webtoon_name_array(data)
+    q = matrix[webtoon_to_index[wt1]]
+    wt_ind = matrix[webtoon_to_index[wt]]
+    num = np.dot(wt1_ind, mov2_ind)
+    den = np.dot(np.linalg.norm(wt1_ind), np.linalg.norm(mov2_ind))
+
+    return num/den
+
 def sqlalchemy_search():
-    #webtoons = [webtoon.simple_serialize() for webtoon in Webtoon.query.all()]
+    summaries = [webtoon.simple_serialize()["summary"] for webtoon in Webtoon.query.all()]
     query_input = request.args.get("q")
     webtoons = Webtoon.query.all()
     cosine_scores = {}
