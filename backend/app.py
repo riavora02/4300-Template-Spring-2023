c6aedfbf35662e26236330afcdab284780b4af5c
Hannah Daniel <hcd38@cornell.edu>
2023-03-27 21:35:02 -0400

    edit

--- backend/app.py
+++ backend/app.py
@@ -198,7 +198,7 @@ def sqlalchemy_search():
     results = get_cossim(webtoons,query_input)
     web_ind = make_index(webtoons)
     for i in range(len(results),-1,-1):
-        output.append(web_ind[results[i]])
+        output.append(web_ind[results[i][1]])
     
     return success_response({"webtoons": output[:10]})
 
