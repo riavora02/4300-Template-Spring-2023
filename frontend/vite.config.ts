24d49b05b38a08fce67d10187a9cb79b7228876f
mluo24 <miranda.luo24@gmail.com>
2023-03-27 23:48:35 -0400

    delete unnecessary files

--- frontend/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import VueTypeImports from 'vite-plugin-vue-type-imports'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), VueTypeImports()],
-})
