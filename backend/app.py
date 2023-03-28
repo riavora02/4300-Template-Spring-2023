4208780e0492b77f20748d77fd3d38548b857f86
Hannah Daniel <hcd38@cornell.edu>
2023-03-27 21:30:16 -0400

    regex import

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ import math
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer 
 import nltk
+import re
 from nltk.tokenize import TreebankWordTokenizer
 from flask import Flask, render_template, request
 from flask_cors import CORS
