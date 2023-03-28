ce1cd7352ac206996722b70f51643ea1bc14b31c
mluo24 <miranda.luo24@gmail.com>
2023-03-28 00:09:08 -0400

    Revert "temporarily remove tailwind"
    This reverts commit 063ba210a3b46e6078c76c8930ce48bf3368cd13.

---
+++ backend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./static/src/**/*.js",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'display': ['Lexend'],
+    }
+  },
+  plugins: []
+}
