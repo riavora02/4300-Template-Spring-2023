99538d1523ac16daf8f24e9f6fe67ce258d870e1
mluo24 <miranda.luo24@gmail.com>
2023-05-01 20:12:58 -0400

    a lot of styling with colors

--- frontend/src/components/Webtoon.vue
+++ frontend/src/components/Webtoon.vue
@@ -2,6 +2,8 @@
 import { WebtoonType } from "../types"
 
 defineProps<{ webtoon: WebtoonType }>()
+
+const simToRecommend = () => {}
 </script>
 
 <template>
@@ -11,18 +13,121 @@ defineProps<{ webtoon: WebtoonType }>()
       <p>Rerum reiciendis beatae tenetur excepturi</p>
     </div>
   </div> -->
-  <div class="card border border-gray-200 shadow-lg rounded-lg">
+  <!-- border border-gray-200 shadow-lg rounded-lg -->
+  <div>
     <div class="card-body">
-      <h2 class="card-title">
-        <a :href="webtoon.url" target="_blank">{{ webtoon.title }}</a>
-      </h2>
-      <div class="p-6 text-slate-800">
+      <div class="card-title">
+        <a
+          class="text-3xl uppercase underline underline-offset-8"
+          :href="webtoon.url"
+          target="_blank"
+        >
+          {{ webtoon.title }}
+        </a>
+        <div>
+          <div class="chat chat-start">
+            <div class="chat-bubble">{{ webtoon.genre }}</div>
+          </div>
+        </div>
+        <span>{{ webtoon.sim.toPrecision(2) }}</span>
+      </div>
+      <div class="flex gap-2">
+        <div class="flex gap-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
+            />
+          </svg>
+          <span>
+            {{ webtoon.released_date }}
+          </span>
+        </div>
+        <div class="flex gap-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="red"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+            />
+          </svg>
+          <span>{{ webtoon.likes }}</span>
+        </div>
+        <div class="flex gap-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"
+            />
+          </svg>
+
+          {{ webtoon.subscribe }}
+        </div>
+        <div class="flex gap-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+            />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+          </svg>
+          {{ webtoon.view }}
+        </div>
+        <div class="flex gap-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="yellow"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6 text-yellow-400"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+            />
+          </svg>
+          <span>{{ webtoon.grade }}</span>
+        </div>
+      </div>
+      <div>
         <p>{{ webtoon.summary }}</p>
-        <p>{{ webtoon.released_date }}</p>
-        <p>{{ webtoon.likes }}</p>
-        <p>{{ webtoon.subscribe }}</p>
-        <p>{{ webtoon.view }}</p>
-        <p>{{ webtoon.sim }}</p>
       </div>
     </div>
     <!-- <div class="w-full p-4 bg-green-200">
